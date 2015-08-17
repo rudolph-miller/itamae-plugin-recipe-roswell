0eebac8ad81cd70bdadb8c39bddfce11b3b3842d
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-08-18 05:25:19 +0900

    gem install diff-lcs -v 1.2.5.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'itamae/plugin/recipe/roswell'
+
+require 'docker'
+require 'serverspec'
+
+set :backend, :docker
+set :docker_url, ENV['DOCKER_HOST']
+set :docker_image, 'rudolphm/roswell'
+
+Excon.defaults[:ssl_verify_peer] = false
